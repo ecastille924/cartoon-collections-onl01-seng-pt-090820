b1341b595f7606e71d5fa8767a9ee6e3d4a47b68
Erick Castille <ecastille924@gmail.com>
2020-09-18 23:57:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,9 @@ def roll_call_dwarves(array)
 end
 
 
-def summon_captain_planet
+def summon_captain_planet(array)
+  planeteer_calls = [ ]
+  
 end
 
 def long_planeteer_calls# code an argument here
