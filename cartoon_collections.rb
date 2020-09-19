99211e4623a542ca2dc977363623b4275d59e42e
Erick Castille <ecastille924@gmail.com>
2020-09-19 00:00:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,9 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese#(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
