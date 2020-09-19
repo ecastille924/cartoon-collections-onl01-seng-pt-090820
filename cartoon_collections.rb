6419f9fedfd97c0e8fce5c1e014ba044b603efb6
Erick Castille <ecastille924@gmail.com>
2020-09-19 00:02:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,8 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese#(array)
+#def find_the_cheese(array)
   array.find do |cheese|
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
-  cheese_types = ["cheddar", "gouda", "camembert"]
 end
